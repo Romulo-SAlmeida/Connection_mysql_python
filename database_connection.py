2b7ba59ea65c0a1aff69405fabeff72039292b6f
Romulo-SAlmeida <raaalmeida9@gmail.com>
2024-03-26 22:16:52 -0300

    ...

---
+++ database_connection.py
@@ -0,0 +1,9 @@
+
+class MySQLDatabase:
+    def __init__(self):
+        self._host =
+        self._username =
+        self._password =
+        self._dabase =
+        self.conn =
+    pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from database_connection import MySQLDatabase
+
+if __name__ == '__main__':
+    print("Runing")
